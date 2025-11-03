0d5e012b7631b7aa8a0cc99db044ac671d96d841
Simonongst <simonongst@gmail.com>
2025-11-03 22:40:02 +0800

    feat: implement create, read, update, delete, and parent lookup for embedded subtasks

--- models/todo.js
+++ models/todo.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const subtaskSchema = new mongoose.Schema({
-    text: String,
-    isComplete: Boolean,
+  text: String,
+  isComplete: Boolean,
 });
 
 const todoSchema = new mongoose.Schema({
-    text: String,
-    isComplete: Boolean,
-    subtasks: [subtaskSchema], // embedded subtask schema
+  text: String,
+  isComplete: Boolean,
+  subtasks: [subtaskSchema], // embedded subtask schema
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
--- queries.js
+++ queries.js
@@ -34,9 +34,77 @@ const findTodos = async () => {
     console.log('All todo:', todos);
 };
 
+const createSubtask = async () => {
+  // Assume that the todo we want to create a
+  // sub-task for has the following id:
+  const todoId = "6908b340aa42360b5575db74";
+  // Look up the todo by id, assign the returned object to `todo`
+  const todo = await Todo.findById(todoId);
+
+  const subtaskData = {
+    text: "Learn how props work",
+    isComplete: false,
+  };
+
+  // Push the new sub-task data into the subtasks array on the todo:
+  const subtask = todo.subtasks.push(subtaskData);
+  // Save the parent document:
+  await todo.save();
+  console.log("Modified todo:", todo);
+};
+
+const findSubtask = async () => {
+    const todoId = '6908b340aa42360b5575db74';
+    const subtaskId = '6908b6d66255d32083273378';
+
+    const todo = await Todo.findById(todoId);
+    const subtask = todo.subtasks.id(subtaskId);
+
+    console.log('Subdocument', subtask);
+};
+
+const removeSubtask = async () => {
+    const todoId = '6908b340aa42360b5575db74';
+    const subtaskId = '6908b6d66255d32083273378';
+
+    const todo = await Todo.findById(todoId);
+    todo.subtasks.pull(subtaskId);
+    await todo.save();
+
+    console.log('Updated document', todo);
+};
+
+const updateSubtask = async () => {
+    const todoId = '6908b340aa42360b5575db74';
+    const subtaskId = '6908ba1c441ba78a3a35298e';
+
+    const todo = await Todo.findById(todoId);
+    const subtask = todo.subtasks.id(subtaskId);
+
+    subtask.isComplete = true;
+    await todo.save();
+
+    console.log('Updated document', todo);
+};
+
+const findParentAndRemoveSubtask = async () => {
+    const foundTodo = await Todo.findOne({
+        'subtasks.text': 'Learn how props work'
+    });
+
+    const foundSubtask = foundTodo.subtasks.find((subtask) => {
+        return subtask.text === 'Learn how props work'
+    });
+
+    foundSubtask.deleteOne();
+
+    await foundTodo.save();
+    console.log('Updated todo:', foundTodo);
+};
+
 /*------------------------------- Run Queries -------------------------------*/
 
 const runQueries = async () => {
-    console.log('Queries running.');
-    await createTodo();
-}
+  console.log("Queries running.");
+  await findParentAndRemoveSubtask();
+};
